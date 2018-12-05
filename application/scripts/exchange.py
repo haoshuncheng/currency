a193687afe689f6ecf93b32898a6daccc81015f6
haosuncheng <haoshuncheng@ivymobile.com>
2018-12-05 13:43:43 +0800

    u

--- application/scripts/exchange.py
+++ application/scripts/exchange.py
@@ -45,7 +45,7 @@ def get_data(i, headers, connect):
 		country = record.xpath("./td[5]/a/text()")
 		country = country[0] if len(transaction_pair) else ''
 
-		transaction_types = record.xpath("./td[5]/")
+		transaction_types = record.xpath("./td[5]")
 
 		print(transaction_types)
 		
