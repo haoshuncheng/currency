c241ff58f65e0635676a9a63883cccda859acb13
haosuncheng <haoshuncheng@ivymobile.com>
2018-12-06 11:57:49 +0800

    u

--- application/scripts/volume.py
+++ application/scripts/volume.py
@@ -3,18 +3,18 @@ from lxml import etree
 import pymysql.cursors
 import sys
 import time
-
+from common import *
 def main():
 	headers = {'content-type': 'application/json', 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0'}
 	connect = connect1()
 	date = time.strftime("%Y-%m-%d", time.localtime())
-
-	get_data(headers, connect, date)
+	for d_type in ['vol_exchange','vol_coin']:
+		get_data(headers, connect, d_type)
 	# f = open('./abc.html', 'w', encoding='utf-8')
 	# f.write(rs.text)
 	# f.close()
 
-def get_data(headers, connect, date):
+def get_data(headers, connect, d_type):
 	rs = requests.get('https://www.feixiaohao.com', headers=headers)
 	#rs = requests.get('https://www.feixiaohao.com/list_1.html', headers=headers)
 	print(rs.status_code)
@@ -22,11 +22,8 @@ def get_data(headers, connect, date):
 		print("数据请求失败\n")
 		return
 	tree = etree.HTML(rs.text)
-	r = tree.xpath('//table[@id="vol_coin"]/tbody/tr')
-	#r = tree.xpath('//table[@id="table"]//tr/td[2]/a/img/@alt')
-	#print(r)
+	r = tree.xpath('//table[@id="'+d_type+'"]/tbody/tr')
 	for record in r:
-
 		rank = record.xpath("./td[1]/span/text()")
 		rank = rank[0] if len(rank) else 0
 		href = record.xpath("./td[2]/a/@href")
@@ -35,13 +32,15 @@ def get_data(headers, connect, date):
 		icon = icon[0] if len(icon) else ''
 		name = record.xpath("./td[2]/a/text()")
 		name = name[1] if len(name) else ''
-		print([rank,href,icon,name])		
+		data_type = d_type
+		rp_date = getTime(0,'-')
+		# print([rank,href,icon,name])		
 
-		# sql = "REPLACE INTO currency_data (rp_date,number,name,icon,market_cap_usd,market_cap_cny,market_cap_btc,price_usd,price_cny,num,volume_usd,volume_cny,volume_btc,text_red,char_line) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')"
-		# data = (str(date),int(number),str(name),str(icon),str(market_cap_usd),str(market_cap_cny),str(market_cap_btc),str(price_usd),str(price_cny),str(num),str(volume_usd),str(volume_cny),str(volume_btc),str(text_red),str(char_line))
-		# connect['cur'].execute(sql % data)
-		# connect['con'].commit()
-		# print('成功插入', connect['cur'].rowcount, '条数据')
+		sql = "REPLACE INTO volume (rp_date,name,icon,rank,data_type,href) VALUES ('%s','%s','%s',%s,'%s','%s')"
+		data = (rp_date,name,icon,rank,data_type,html)
+		connect['cur'].execute(sql % data)
+		connect['con'].commit()
+		print('成功插入', connect['cur'].rowcount, '条数据')
 		#print(alt)
 	#print(len(r))
 
