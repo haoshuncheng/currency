de26821069a8d90dff5be08dd7731cac564d6bc1
haosuncheng <haoshuncheng@ivymobile.com>
2018-12-24 15:37:11 +0800

    u

--- application/scripts/currency/test.py
+++ application/scripts/currency/test.py
@@ -1,16 +1,28 @@
 import requests
 import numpy as np
-
+import time
+from rank_data import *
 def run():
-	global baseurl
-	rs = requests.get(baseurl)
-	data = rs.json()
-	record = [float(x['price']) for x in data]
-	avg = np.mean(record)
-	print(avg)
+	connect = connect1()
+	cursor = connect['cur']
+	connect = connect['con']
+	url = 'https://api.binance.com/api/v1/ticker/price'
+	data = get_json(url)
+	if not data:
+		return
+	coin_avg = np.mean([float(x['price']) for x in data])
+	coin_type = 1
+	coint_second = time.time()
+	sql = "insert into coin_price(type,price,second) values(%s,%s,%s)"
+	cursor.execute(sql,[coin_type,coin_price,coint_second])
+	connect.commit()
+
+def get_json(url):
+	rs = requests.get(url)
+	if rs.status == requests.status_code:
+		return rs.json()
 
 
 
 if __name__ == '__main__':
-	baseurl = 'https://api.binance.com/api/v1/ticker/price'
 	run()
