b5915215106faae9ce3c2a4c8471f426c0b91d5c
haosuncheng <haoshuncheng@ivymobile.com>
2018-12-24 14:56:49 +0800

    u

--- application/scripts/currency/test.py
+++ application/scripts/currency/test.py
@@ -4,11 +4,10 @@ import numpy as np
 def run():
 	global baseurl
 	rs = requests.get(baseurl)
-	rs = rs.json()
-	rs = [float(x['price']) for x in rs]
-	print(rs)
-	x = np.mean(rs)
-	print(x)
+	data = rs.json()
+	record = [float(x['price']) for x in rs]
+	avg = np.mean(record)
+	print(avg)
 
 
 
