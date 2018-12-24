e62cc18554963bb993b2fd79b09a28dfccc47e5b
haosuncheng <haoshuncheng@ivymobile.com>
2018-12-24 15:52:05 +0800

    u

--- application/scripts/currency/test.py
+++ application/scripts/currency/test.py
@@ -6,9 +6,9 @@ def run():
 	data = get_json(url)
 	if not data:
 		return
-	coin_avg = str(np.mean([float(x['price']) for x in data]))
 	coin_type = 1
 	coint_second = str(int(time.time()))
+	coin_avg = str(np.mean([float(x['price']) for x in data]))
 	print([coin_type,coin_avg,coint_second])
 	sql = "insert into coin_price(type,price,second) values("+str(coin_type)+",'"+coin_avg+"','"+coint_second+"')"
 	cursor.execute(sql)
@@ -21,7 +21,6 @@ def get_json(url):
 		return rs.json()
 
 
-
 if __name__ == '__main__':
 	connect = connect1()
 	cursor = connect['cur']
@@ -29,4 +28,4 @@ if __name__ == '__main__':
 	url = 'https://api.binance.com/api/v1/ticker/price'
 	while True:
 		run()
-		time.sleep(1)
+		time.sleep(3)
