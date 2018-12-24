4e67ffb5a370432eb1646865cea9d4b0b663b25f
haosuncheng <haoshuncheng@ivymobile.com>
2018-12-24 17:20:10 +0800

    u

--- application/scripts/currency/coin_k.py
+++ application/scripts/currency/coin_k.py
@@ -8,7 +8,7 @@ def run(time_type):
 	if time_type == 'MIN':
 		end_time = int(time.time())
 		st_time = end_time-60
-		sql = "select price from coin_price where second > '"+str(st_time)+"' and second < '"+str(end_time)+"' order by second asc"
+		sql = "select price from coin_price where second > '"+str(st_time)+"' and second < '"+str(end_time)+"' and type=1 order by second asc"
 		cursor.execute(sql)
 		rs = cursor.fetchall()
 		record = [float(x['price']) for x in rs]
@@ -21,7 +21,151 @@ def run(time_type):
 		print(sql)
 		cursor.execute(sql)
 		connect.commit()
-		
+	if time_type == 'M5':
+		end_time = int(time.time())
+		st_time = end_time-300
+		sql = "select price from line_data_sp where from=1 and epochSecond > '"+str(st_time)+"' and epochSecond < '"+str(end_time)+"' order by epochSecond asc"
+		cursor.execute(sql)
+		rs = cursor.fetchall()
+		record = [float(x['price']) for x in rs]
+		st = record[0]
+		end = record[-1]
+		mx = max(record)
+		mn = min(record)
+		price = np.mean(record)
+		sql = "replace into line_data_sp (`epochSecond`,`type`,`high`,`low`,`open`,`close`,`from`,`price`) values('"+str(st_time)+"','"+time_type+"','"+str(mx)+"','"+str(mn)+"','"+str(st)+"','"+str(end)+"',1,'"+str(price)+"')"
+		print(sql)
+		cursor.execute(sql)
+		connect.commit()
+	if time_type == 'M15':
+		end_time = int(time.time())
+		st_time = end_time-15*60
+		sql = "select price from line_data_sp where from=1 and epochSecond > '"+str(st_time)+"' and epochSecond < '"+str(end_time)+"' order by epochSecond asc"
+		cursor.execute(sql)
+		rs = cursor.fetchall()
+		record = [float(x['price']) for x in rs]
+		st = record[0]
+		end = record[-1]
+		mx = max(record)
+		mn = min(record)
+		price = np.mean(record)
+		sql = "replace into line_data_sp (`epochSecond`,`type`,`high`,`low`,`open`,`close`,`from`,`price`) values('"+str(st_time)+"','"+time_type+"','"+str(mx)+"','"+str(mn)+"','"+str(st)+"','"+str(end)+"',1,'"+str(price)+"')"
+		print(sql)
+		cursor.execute(sql)
+		connect.commit()
+	if time_type == 'M30':
+		end_time = int(time.time())
+		st_time = end_time-30*60
+		sql = "select price from line_data_sp where from=1 and epochSecond > '"+str(st_time)+"' and epochSecond < '"+str(end_time)+"' order by epochSecond asc"
+		cursor.execute(sql)
+		rs = cursor.fetchall()
+		record = [float(x['price']) for x in rs]
+		st = record[0]
+		end = record[-1]
+		mx = max(record)
+		mn = min(record)
+		price = np.mean(record)
+		sql = "replace into line_data_sp (`epochSecond`,`type`,`high`,`low`,`open`,`close`,`from`,`price`) values('"+str(st_time)+"','"+time_type+"','"+str(mx)+"','"+str(mn)+"','"+str(st)+"','"+str(end)+"',1,'"+str(price)+"')"
+		print(sql)
+		cursor.execute(sql)
+		connect.commit()
+	if time_type == 'HOUR':
+		end_time = int(time.time())
+		st_time = end_time-60*60
+		sql = "select price from line_data_sp where from=1 and epochSecond > '"+str(st_time)+"' and epochSecond < '"+str(end_time)+"' order by epochSecond asc"
+		cursor.execute(sql)
+		rs = cursor.fetchall()
+		record = [float(x['price']) for x in rs]
+		st = record[0]
+		end = record[-1]
+		mx = max(record)
+		mn = min(record)
+		price = np.mean(record)
+		sql = "replace into line_data_sp (`epochSecond`,`type`,`high`,`low`,`open`,`close`,`from`,`price`) values('"+str(st_time)+"','"+time_type+"','"+str(mx)+"','"+str(mn)+"','"+str(st)+"','"+str(end)+"',1,'"+str(price)+"')"
+		print(sql)
+		cursor.execute(sql)
+		connect.commit()
+	if time_type == 'H2':
+		end_time = int(time.time())
+		st_time = end_time-120*60
+		sql = "select price from line_data_sp where from=1 and epochSecond > '"+str(st_time)+"' and epochSecond < '"+str(end_time)+"' order by epochSecond asc"
+		cursor.execute(sql)
+		rs = cursor.fetchall()
+		record = [float(x['price']) for x in rs]
+		st = record[0]
+		end = record[-1]
+		mx = max(record)
+		mn = min(record)
+		price = np.mean(record)
+		sql = "replace into line_data_sp (`epochSecond`,`type`,`high`,`low`,`open`,`close`,`from`,`price`) values('"+str(st_time)+"','"+time_type+"','"+str(mx)+"','"+str(mn)+"','"+str(st)+"','"+str(end)+"',1,'"+str(price)+"')"
+		print(sql)
+		cursor.execute(sql)
+		connect.commit()
+	if time_type == 'H4':
+		end_time = int(time.time())
+		st_time = end_time-4*60*60
+		sql = "select price from line_data_sp where from=1 and epochSecond > '"+str(st_time)+"' and epochSecond < '"+str(end_time)+"' order by epochSecond asc"
+		cursor.execute(sql)
+		rs = cursor.fetchall()
+		record = [float(x['price']) for x in rs]
+		st = record[0]
+		end = record[-1]
+		mx = max(record)
+		mn = min(record)
+		price = np.mean(record)
+		sql = "replace into line_data_sp (`epochSecond`,`type`,`high`,`low`,`open`,`close`,`from`,`price`) values('"+str(st_time)+"','"+time_type+"','"+str(mx)+"','"+str(mn)+"','"+str(st)+"','"+str(end)+"',1,'"+str(price)+"')"
+		print(sql)
+		cursor.execute(sql)
+		connect.commit()
+	if time_type == 'H6':
+		end_time = int(time.time())
+		st_time = end_time-60*60*6
+		sql = "select price from line_data_sp where from=1 and epochSecond > '"+str(st_time)+"' and epochSecond < '"+str(end_time)+"' order by epochSecond asc"
+		cursor.execute(sql)
+		rs = cursor.fetchall()
+		record = [float(x['price']) for x in rs]
+		st = record[0]
+		end = record[-1]
+		mx = max(record)
+		mn = min(record)
+		price = np.mean(record)
+		sql = "replace into line_data_sp (`epochSecond`,`type`,`high`,`low`,`open`,`close`,`from`,`price`) values('"+str(st_time)+"','"+time_type+"','"+str(mx)+"','"+str(mn)+"','"+str(st)+"','"+str(end)+"',1,'"+str(price)+"')"
+		print(sql)
+		cursor.execute(sql)
+		connect.commit()
+	if time_type == 'H12':
+		end_time = int(time.time())
+		st_time = end_time-60*60*12
+		sql = "select price from line_data_sp where from=1 and epochSecond > '"+str(st_time)+"' and epochSecond < '"+str(end_time)+"' order by epochSecond asc"
+		cursor.execute(sql)
+		rs = cursor.fetchall()
+		record = [float(x['price']) for x in rs]
+		st = record[0]
+		end = record[-1]
+		mx = max(record)
+		mn = min(record)
+		price = np.mean(record)
+		sql = "replace into line_data_sp (`epochSecond`,`type`,`high`,`low`,`open`,`close`,`from`,`price`) values('"+str(st_time)+"','"+time_type+"','"+str(mx)+"','"+str(mn)+"','"+str(st)+"','"+str(end)+"',1,'"+str(price)+"')"
+		print(sql)
+		cursor.execute(sql)
+		connect.commit()
+	if time_type == 'DAY':
+		end_time = int(time.time())
+		st_time = end_time-60*60*24
+		sql = "select price from line_data_sp where from=1 and epochSecond > '"+str(st_time)+"' and epochSecond < '"+str(end_time)+"' order by epochSecond asc"
+		cursor.execute(sql)
+		rs = cursor.fetchall()
+		record = [float(x['price']) for x in rs]
+		st = record[0]
+		end = record[-1]
+		mx = max(record)
+		mn = min(record)
+		price = np.mean(record)
+		sql = "replace into line_data_sp (`epochSecond`,`type`,`high`,`low`,`open`,`close`,`from`,`price`) values('"+str(st_time)+"','"+time_type+"','"+str(mx)+"','"+str(mn)+"','"+str(st)+"','"+str(end)+"',1,'"+str(price)+"')"
+		print(sql)
+		cursor.execute(sql)
+		connect.commit()
+
 		
 
 
