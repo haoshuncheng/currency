b2490d9a4d024b57b1877a2cdd56fbcafe0b1018
haosuncheng <haoshuncheng@ivymobile.com>
2019-01-13 16:00:45 +0800

    u

--- application/scripts/currency/get_exchange_list.py
+++ application/scripts/currency/get_exchange_list.py
@@ -42,21 +42,21 @@ def get_list(url,isinnovation):
 			cursor.execute("REPLACE INTO exchangetrades(code,info) values('"+record['id']+"','"+exchangetrades_data+"')")
 			connect['con'].commit()
 
-			coin_pairs_url = "https://dncapi.feixiaohao.com/api/exchange/coinpair_list"
-			print(coin_pairs_url)
-			data = {"code":record['id'],"page":1,"pagesize":1000,"webp":1}
-			coin_pairs_rs = post_requests(coin_pairs_url,json.dumps(data))
-			if coin_pairs_rs == False:
-				print("list列表失败\n")
-				continue
-			if 'code' not in coin_pairs_rs or str(coin_pairs_rs['code'])!='200' or 'data' not in coin_pairs_rs or len(coin_pairs_rs['data'])==0:
-				print(data)
-				print("json数据异常\n")
-				continue
-			coin_pairs_data = coin_pairs_rs['data']
-			for coin_record in coin_pairs_data:
-				coin_record['code'] = record['id']
-				write(connect['con'],'coin_pairs',coin_record)
+			# coin_pairs_url = "https://dncapi.feixiaohao.com/api/exchange/coinpair_list"
+			# print(coin_pairs_url)
+			# data = {"code":record['id'],"page":1,"pagesize":1000,"webp":1}
+			# coin_pairs_rs = post_requests(coin_pairs_url,json.dumps(data))
+			# if coin_pairs_rs == False:
+			# 	print("list列表失败\n")
+			# 	continue
+			# if 'code' not in coin_pairs_rs or str(coin_pairs_rs['code'])!='200' or 'data' not in coin_pairs_rs or len(coin_pairs_rs['data'])==0:
+			# 	print(data)
+			# 	print("json数据异常\n")
+			# 	continue
+			# coin_pairs_data = coin_pairs_rs['data']
+			# for coin_record in coin_pairs_data:
+			# 	coin_record['code'] = record['id']
+			# 	write(connect['con'],'coin_pairs',coin_record)
 
 			# exchangeinfo_url = "https://dncapi.feixiaohao.com/api/exchange/exchangeinfo?code="+record['id']+"&webp=1"
 			# exchangeinfo = get_requests(exchangeinfo_url, 'json')
@@ -85,13 +85,14 @@ def get_list(url,isinnovation):
 			params2 = params2[0].split(",")
 			score = re.findall(r"score\:(\{.*\})\,Currencies",t)
 			score ={r.split(":")[0]:r.split(":")[1]  for r in  score[0].split(",")}
+			print(score)
 			for k,y in score.items():
 				if y in params:
 					score[k] = params2[params.index(y)]
 			print(score)
-			cursor = connect['cur']
-			cursor.execute("REPLACE INTO exchangescore(code,info) values('"+record['id']+"','"+json.dumps(score)+"')")
-			connect['con'].commit()
+			# cursor = connect['cur']
+			# cursor.execute("REPLACE INTO exchangescore(code,info) values('"+record['id']+"','"+json.dumps(score)+"')")
+			# connect['con'].commit()
 		except:
 			s=sys.exc_info()
 			print ("Error '%s' happened on line %d" % (s[1],s[2].tb_lineno))
