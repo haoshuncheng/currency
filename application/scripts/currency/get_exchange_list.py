cb705e3e56c38cf34513552f4941a2d212822c20
haosuncheng <haoshuncheng@ivymobile.com>
2019-01-13 16:46:16 +0800

    u

--- application/scripts/currency/get_exchange_list.py
+++ application/scripts/currency/get_exchange_list.py
@@ -98,9 +98,8 @@ def get_list(url,isinnovation):
 
 			pairData = re.findall(r"pairData\:\[(.*)\]\,basePairData",t)
 			pairData = [r for r in pairData[0].split(",")]
+			pairData = [ r.replace("{","").replace("}","") for r in pairData]
 			print(pairData)
-			
-
 		except:
 			s=sys.exc_info()
 			print ("Error '%s' happened on line %d" % (s[1],s[2].tb_lineno))
