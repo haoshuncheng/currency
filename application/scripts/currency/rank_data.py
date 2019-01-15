01cccc1d6e8da29fd2141f3b5205c977dabafc4e
haoshuncheng <chenghao900@163.com>
2019-01-15 21:09:19 +0800

    u

--- application/scripts/currency/rank_data.py
+++ application/scripts/currency/rank_data.py
@@ -63,6 +63,8 @@ def main():
 			return False
 		html = etree.HTML(rs)
 
+		print(html)
+
 		turnover = html.xpath("//dd[@class='last-dd'][1]/span/text()")[0]
 		flow_rate = html.xpath("//dd[@class='last-dd'][2]/span/text()")[0]
 
